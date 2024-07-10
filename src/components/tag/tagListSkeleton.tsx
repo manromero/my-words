500d80003d2486140974d8a679320b82f17c33c6
manromero <migu621@gmail.com>
2024-07-10 17:38:15 +0200

    improve skeleton

--- src/components/tag/tagListSkeleton.tsx
+++ src/components/tag/tagListSkeleton.tsx
@@ -3,46 +3,54 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import Divider from "@mui/material/Divider";
 import React from "react";
-import { Chip, Skeleton } from "@mui/material";
-import { TagType } from "@/types";
+import { Skeleton } from "@mui/material";
 
 type TagListSkeletonType = {};
 
 export const TagListSkeleton = (props: TagListSkeletonType) => {
   return (
     <List>
-      <React.Fragment>
-        <Divider variant="fullWidth" component="li" />
-        <ListItem disablePadding>
-          <ListItemButton>
-            <Skeleton variant="rounded" width={130} height={32} />
-          </ListItemButton>
-        </ListItem>
-      </React.Fragment>
-      <React.Fragment>
-        <Divider variant="fullWidth" component="li" />
-        <ListItem disablePadding>
-          <ListItemButton>
-            <Skeleton variant="rounded" width={220} height={32} />
-          </ListItemButton>
-        </ListItem>
-      </React.Fragment>
-      <React.Fragment>
-        <Divider variant="fullWidth" component="li" />
-        <ListItem disablePadding>
-          <ListItemButton>
-            <Skeleton variant="rounded" width={100} height={32} />
-          </ListItemButton>
-        </ListItem>
-      </React.Fragment>
-      <React.Fragment>
-        <Divider variant="fullWidth" component="li" />
-        <ListItem disablePadding>
-          <ListItemButton>
-            <Skeleton variant="rounded" width={150} height={32} />
-          </ListItemButton>
-        </ListItem>
-      </React.Fragment>
+      <TagListSkeletonGroup />
+      <TagListSkeletonGroup />
+      <TagListSkeletonGroup />
+      <TagListSkeletonGroup />
     </List>
   );
 };
+
+const TagListSkeletonGroup = () => (
+  <React.Fragment>
+    <React.Fragment>
+      <Divider variant="fullWidth" component="li" />
+      <ListItem disablePadding>
+        <ListItemButton>
+          <Skeleton variant="rounded" width={130} height={32} />
+        </ListItemButton>
+      </ListItem>
+    </React.Fragment>
+    <React.Fragment>
+      <Divider variant="fullWidth" component="li" />
+      <ListItem disablePadding>
+        <ListItemButton>
+          <Skeleton variant="rounded" width={220} height={32} />
+        </ListItemButton>
+      </ListItem>
+    </React.Fragment>
+    <React.Fragment>
+      <Divider variant="fullWidth" component="li" />
+      <ListItem disablePadding>
+        <ListItemButton>
+          <Skeleton variant="rounded" width={100} height={32} />
+        </ListItemButton>
+      </ListItem>
+    </React.Fragment>
+    <React.Fragment>
+      <Divider variant="fullWidth" component="li" />
+      <ListItem disablePadding>
+        <ListItemButton>
+          <Skeleton variant="rounded" width={150} height={32} />
+        </ListItemButton>
+      </ListItem>
+    </React.Fragment>
+  </React.Fragment>
+);
--- src/components/word/wordListSkeleton.tsx
+++ src/components/word/wordListSkeleton.tsx
@@ -11,114 +11,123 @@ type WordListSkeletonType = {};
 export const WordListSkeleton = (props: WordListSkeletonType) => {
   return (
     <List>
-      <React.Fragment>
-        <Divider variant="fullWidth" component="li" />
-        <ListItem disablePadding>
-          <ListItemButton
-            sx={{
-              flexDirection: "column",
-              alignItems: "flex-start",
-            }}
-          >
-            <ListItemText
-              primary={<Skeleton variant="rounded" width={150} height={24} />}
-              secondary={
-                <Skeleton
-                  variant="rounded"
-                  width={200}
-                  height={20}
-                  sx={{ marginTop: 1 }}
-                />
-              }
-            />
-            <Stack direction="row" gap={1} flexWrap="wrap">
-              <Skeleton variant="rounded" width={70} height={32} />
-            </Stack>
-          </ListItemButton>
-        </ListItem>
-      </React.Fragment>
-      <React.Fragment>
-        <Divider variant="fullWidth" component="li" />
-        <ListItem disablePadding>
-          <ListItemButton
-            sx={{
-              flexDirection: "column",
-              alignItems: "flex-start",
-            }}
-          >
-            <ListItemText
-              primary={<Skeleton variant="rounded" width={90} height={24} />}
-              secondary={
-                <Skeleton
-                  variant="rounded"
-                  width={100}
-                  height={20}
-                  sx={{ marginTop: 1 }}
-                />
-              }
-            />
-            <Stack direction="row" gap={1} flexWrap="wrap">
-              <Skeleton variant="rounded" width={80} height={32} />
-              <Skeleton variant="rounded" width={70} height={32} />
-            </Stack>
-          </ListItemButton>
-        </ListItem>
-      </React.Fragment>
-      <React.Fragment>
-        <Divider variant="fullWidth" component="li" />
-        <ListItem disablePadding>
-          <ListItemButton
-            sx={{
-              flexDirection: "column",
-              alignItems: "flex-start",
-            }}
-          >
-            <ListItemText
-              primary={<Skeleton variant="rounded" width={120} height={24} />}
-              secondary={
-                <Skeleton
-                  variant="rounded"
-                  width={160}
-                  height={20}
-                  sx={{ marginTop: 1 }}
-                />
-              }
-            />
-            <Stack direction="row" gap={1} flexWrap="wrap">
-              <Skeleton variant="rounded" width={70} height={32} />
-              <Skeleton variant="rounded" width={60} height={32} />
-              <Skeleton variant="rounded" width={80} height={32} />
-            </Stack>
-          </ListItemButton>
-        </ListItem>
-      </React.Fragment>
-      <React.Fragment>
-        <Divider variant="fullWidth" component="li" />
-        <ListItem disablePadding>
-          <ListItemButton
-            sx={{
-              flexDirection: "column",
-              alignItems: "flex-start",
-            }}
-          >
-            <ListItemText
-              primary={<Skeleton variant="rounded" width={120} height={24} />}
-              secondary={
-                <Skeleton
-                  variant="rounded"
-                  width={160}
-                  height={20}
-                  sx={{ marginTop: 1 }}
-                />
-              }
-            />
-            <Stack direction="row" gap={1} flexWrap="wrap">
-              <Skeleton variant="rounded" width={80} height={32} />
-              <Skeleton variant="rounded" width={70} height={32} />
-            </Stack>
-          </ListItemButton>
-        </ListItem>
-      </React.Fragment>
+      <WordListSkeletonGroup />
+      <WordListSkeletonGroup />
+      <WordListSkeletonGroup />
+      <WordListSkeletonGroup />
     </List>
   );
 };
+
+const WordListSkeletonGroup = () => (
+  <React.Fragment>
+    <React.Fragment>
+      <Divider variant="fullWidth" component="li" />
+      <ListItem disablePadding>
+        <ListItemButton
+          sx={{
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          <ListItemText
+            primary={<Skeleton variant="rounded" width={150} height={24} />}
+            secondary={
+              <Skeleton
+                variant="rounded"
+                width={200}
+                height={20}
+                sx={{ marginTop: 1 }}
+              />
+            }
+          />
+          <Stack direction="row" gap={1} flexWrap="wrap">
+            <Skeleton variant="rounded" width={70} height={32} />
+          </Stack>
+        </ListItemButton>
+      </ListItem>
+    </React.Fragment>
+    <React.Fragment>
+      <Divider variant="fullWidth" component="li" />
+      <ListItem disablePadding>
+        <ListItemButton
+          sx={{
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          <ListItemText
+            primary={<Skeleton variant="rounded" width={90} height={24} />}
+            secondary={
+              <Skeleton
+                variant="rounded"
+                width={100}
+                height={20}
+                sx={{ marginTop: 1 }}
+              />
+            }
+          />
+          <Stack direction="row" gap={1} flexWrap="wrap">
+            <Skeleton variant="rounded" width={80} height={32} />
+            <Skeleton variant="rounded" width={70} height={32} />
+          </Stack>
+        </ListItemButton>
+      </ListItem>
+    </React.Fragment>
+    <React.Fragment>
+      <Divider variant="fullWidth" component="li" />
+      <ListItem disablePadding>
+        <ListItemButton
+          sx={{
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          <ListItemText
+            primary={<Skeleton variant="rounded" width={120} height={24} />}
+            secondary={
+              <Skeleton
+                variant="rounded"
+                width={160}
+                height={20}
+                sx={{ marginTop: 1 }}
+              />
+            }
+          />
+          <Stack direction="row" gap={1} flexWrap="wrap">
+            <Skeleton variant="rounded" width={70} height={32} />
+            <Skeleton variant="rounded" width={60} height={32} />
+            <Skeleton variant="rounded" width={80} height={32} />
+          </Stack>
+        </ListItemButton>
+      </ListItem>
+    </React.Fragment>
+    <React.Fragment>
+      <Divider variant="fullWidth" component="li" />
+      <ListItem disablePadding>
+        <ListItemButton
+          sx={{
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          <ListItemText
+            primary={<Skeleton variant="rounded" width={120} height={24} />}
+            secondary={
+              <Skeleton
+                variant="rounded"
+                width={160}
+                height={20}
+                sx={{ marginTop: 1 }}
+              />
+            }
+          />
+          <Stack direction="row" gap={1} flexWrap="wrap">
+            <Skeleton variant="rounded" width={80} height={32} />
+            <Skeleton variant="rounded" width={70} height={32} />
+          </Stack>
+        </ListItemButton>
+      </ListItem>
+    </React.Fragment>
+  </React.Fragment>
+);
