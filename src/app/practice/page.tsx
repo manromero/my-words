f2f19a9af3b2e523b67c31447877151c7e445eaf
manromero <migu621@gmail.com>
2024-12-12 21:09:52 +0100

    improve practice page structure

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React from "react";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, Collapse, Stack, Typography } from "@mui/material";
 
 import { usePractice } from "@/hooks";
 
@@ -9,15 +9,23 @@ import RestartAltIcon from "@mui/icons-material/RestartAlt";
 import NumbersIcon from "@mui/icons-material/Numbers";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import FlagIcon from "@mui/icons-material/Flag";
+import { WordList } from "@/components";
 export const PlayResume = () => {
   const { restart } = usePractice();
+  const [showWords, setShowWords] = useState(false);
+
+  const handleShowWords = () => {
+    setShowWords(true);
+  };
+
   return (
     <Stack
       direction="column"
       justifyContent="center"
       alignItems="center"
-      marginTop="15%"
-      gap={2}
+      gap={3}
+      width={{ xs: "90%", lg: "60%" }}
+      padding={{ xs: "5vh 5%", lg: "5vh 20%" }}
     >
       <Typography
         variant="h3"
@@ -33,7 +41,7 @@ export const PlayResume = () => {
         flexDirection={{ xs: "column", lg: "row" }}
         gap={2}
         alignItems="stretch"
-        width={{ xs: "80%", lg: "60%" }}
+        width="100%"
       >
         <Box
           display="flex"
@@ -164,6 +172,60 @@ export const PlayResume = () => {
           </Box>
         </Box>
       </Box>
+
+      {!showWords && (
+        <Button
+          variant="outlined"
+          color="primary"
+          type="button"
+          sx={{ width: "100%" }}
+          onClick={handleShowWords}
+        >
+          Show Words
+        </Button>
+      )}
+
+      <Collapse in={showWords} sx={{ width: "100%" }}>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+          }}
+        >
+          <Typography
+            variant="h5"
+            component="h4"
+            fontWeight="900"
+            color="#d32f2f"
+          >
+            Errors
+          </Typography>
+          <WordList
+            words={[
+              { word: "test", translation: "test", tags: ["test"] },
+              { word: "test", translation: "test", tags: ["test"] },
+            ]}
+            tags={[{ id: "test", label: "test" }]}
+          />
+          <Typography
+            variant="h5"
+            component="h4"
+            fontWeight="900"
+            color="#84cf31"
+          >
+            Success
+          </Typography>
+          <WordList
+            words={[
+              { word: "test", translation: "test", tags: ["test"] },
+              { word: "test", translation: "test", tags: ["test"] },
+            ]}
+            tags={[{ id: "test", label: "test" }]}
+          />
+        </Box>
+      </Collapse>
 
       <Button
         variant="contained"
